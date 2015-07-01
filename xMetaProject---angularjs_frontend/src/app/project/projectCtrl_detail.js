a3123b32d7e5075e3699c0b5959bd46c862d5658
unknown <gionuwe@R344WW8P.Pentasys.de>
2015-07-01 16:31:42 +0200

    added code

--- xMetaProject---angularjs_frontend/src/app/project/projectCtrl_detail.js
+++ xMetaProject---angularjs_frontend/src/app/project/projectCtrl_detail.js
@@ -18,21 +18,7 @@ angular.module('module_project')
     //it should give us all the projects from the REST-Service
     $scope.getProject = function (){
       var successCallback = function (){
-        //console.log($scope.project.members);
-        var tmembers = $scope.project.members;
 
-        for(var i= 0; i < tmembers.length; i++){
-
-          for(var j= 0; j < tusers.length; j++){
-            console.log(tusers[j].id);
-            console.log(tmembers[i].id);
-            if(tmembers[i].id == tusers[j].id){
-              console.log('TEST TEST');
-            } else{
-              console.log('BLABLA');
-            }
-          }
-        }
       };
       var errorCallback = function(){
         //console.log('error');
@@ -67,37 +53,36 @@ angular.module('module_project')
           console.log('BLABLA')
         }
       }
-
     };
 
     $scope.availableUsers();
 
-    //$scope.availableUsers = function(){
-    //  return $scope.users.filter(function(user){
-    //    //console.log( $scope.project.members + "  "+ $scope.members + "   " + user + " " + user.id);
-    //      console.log(user);
-    //      //console.log($scope.project.members.indexOf(user));
-    //    console.log($scope.project.members.indexOf(user));
+    $scope.availableUsers = function(){
+      return $scope.users.filter(function(user){
+        //console.log( $scope.project.members + "  "+ $scope.members + "   " + user + " " + user.id);
+          console.log(user);
+          //console.log($scope.project.members.indexOf(user));
+        console.log($scope.project.members.indexOf(user));
+
+        if($scope.project.members.indexOf(user)){
+
+        }
+
+        return $scope.project.members.indexOf(user);
+      });
+    };
+
+    //$scope.fireMember = function(index){
+    //  $scope.users.push($scope.project.members[index]);
+    //  $scope.project.members.splice(index, 1);
+    //};
     //
-    //    if($scope.project.members.indexOf(user)){
-    //
-    //    }
-    //
-    //    return $scope.project.members.indexOf(user);
-    //  });
+    //$scope.hireMember = function(index){
+    //  $scope.project.members.push($scope.users[index]);
+    //  $scope.users.splice(index, 1);
     //};
 
-    $scope.fireMember = function(index){
-      $scope.users.push($scope.project.members[index]);
-      $scope.project.members.splice(index, 1);
-    };
-
-    $scope.hireMember = function(index){
-      $scope.project.members.push($scope.users[index]);
-      $scope.users.splice(index, 1);
-    };
-
-
+    //getUsers();
     $scope.getProject();
     getUsers();
   }]);
